558c3b4bba058f1f69c135d95e3e40223d5efbce
harsha <hroy345@gmail.com>
2014-09-15 23:24:26 +0530

    Added a program that add numbers

--- sum.py
+++ sum.py
@@ -3,4 +3,4 @@
 a = float(input("enter a number: "))
 b = float(input("enter another number: "))
 sum = a + b
-print("First no. is: %f\nSecond no. is: %f\nSum is: %f" % (a, b, sum))
+print("First no. is: %f \nSecond no. is: %f\nSum is: %f" % (a, b, sum))

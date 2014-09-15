04c9048edf32e32748150b51af7c0668aedcd08d
harsha <hroy345@gmail.com>
2014-09-15 23:11:49 +0530

    program that adds two numbers

---
+++ sum.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+a = float(input("enter a number: "))
+b = float(input("enter another number: "))
+sum = a + b
+print("First no. is: %f\nSecond no. is: %f\nSum is: %f" % (a, b, sum))
